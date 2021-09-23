d68103e6e4dbb7d4c66bcabe1fcf606b30818ff8
Alec Dilanchian <alec.dilanchian@gmail.com>
2021-09-23 16:57:07 -0400

    Adding base project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# buildspace-nft-course-starter
-This is the NFT Minting Course Start Project!
+# _buildspace Mint Your Own NFT Collection
+
+### **Welcome 👋**
+To get started with this course, clone this repo and follow these commands:
+
+1. Run `npm install` at the root of your directory
+2. Run `npm run start` to start the project
+3. Start coding!
+
+### **Questions?**
+Have some questions make sure you head over to your [_buildspace Dashboard](https://app.buildspace.so/courses/CO961ddb5f-f428-4608-9949-a9a2f461eb3f) and link your Discord account so you can get access to helpful channels and your instructor!
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+
+const App = () => {
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
